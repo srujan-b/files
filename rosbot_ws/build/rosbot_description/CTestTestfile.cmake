0c27a4919e239ae2e6a7064ab93b6f8cca2ee57c
srujan-b <srujanb28@gmail.com>
2023-06-16 01:14:41 -0700

    Initial commit

---
+++ rosbot_ws/build/rosbot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/husarion/rosbot_ws/src/rosbot_description
+# Build directory: /home/husarion/rosbot_ws/build/rosbot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ rosbot_ws/build/rosbot_description/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/husarion/rosbot_ws/install/ros_components_description/share/ros_components_description/package.sh"
